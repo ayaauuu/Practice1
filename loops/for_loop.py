0028bc1678de2bc6295d110b2dd7e93affd49530
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:35:21 +0300

    Create for_loop.py

---
+++ loops/for_loop.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 11):
+    print(i)
+
+name = "Python"
+
+for letter in name:
+    print(letter)
